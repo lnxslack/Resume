206cfec219141e49ed5ae5c6d88efdd60c997c7c
lnxslack <wanderley.germano@gmail.com>
2026-06-11 17:21:54 -0300

    Atualizando o Inglês

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Pequenas interações: preencher ano atual
+// Fill in current year for PT and EN placeholders
 (function(){
+  const year = new Date().getFullYear();
   document.querySelectorAll('footer .container p').forEach(p=>{
-    p.innerHTML = p.innerHTML.replace('{{ANO_ATUAL}}', new Date().getFullYear());
+    p.innerHTML = p.innerHTML.replace('{{ANO_ATUAL}}', year).replace('{{CURRENT_YEAR}}', year);
   });
 })();
